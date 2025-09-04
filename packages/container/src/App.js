a107389726d35e5abb50d27a41e5a0245903cadd
jesuscarrasquel <jesusmcc1106@gmail.com>
2025-09-04 12:18:14 +0200

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+
+console.log(mount);
+export default () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+  );
+};
